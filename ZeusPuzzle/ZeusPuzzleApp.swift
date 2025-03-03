ca0271afe69da06f37c1f0b3efe5b0aa8079bc6d
keshew <artyom.korotckov2017@yandex.ru>
2025-03-03 18:33:02 +0500

    Initial Commit

---
+++ ZeusPuzzle/ZeusPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeusPuzzleApp.swift
+//  ZeusPuzzle
+//
+//  Created by Артём Коротков on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZeusPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
